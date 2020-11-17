e5b8169f061279a0269cf40c393183ff58a477bc
andreske02 <andres.vergauwen@hotmail.com>
2020-11-17 20:53:05 +0100

    test(integration): new storyblock added

--- api/src/__tests__/integration.test.js
+++ api/src/__tests__/integration.test.js
@@ -28,3 +28,33 @@ describe('POST /test endpoint', ()=>{
         }
     })
 })
+
+describe('POST /newstoryblock endpoint', ()=>{
+    test('check if response is created and story_id exist', async (done) =>{
+        const service = {
+            content: "Hallo ik ben andres",
+            story_id: 2
+        };
+        try {
+            const response = await request.post('/newstoryblock').send(service)
+            expect(response.status).toBe(200,done())
+        } catch (error) {
+            console.log(error);
+            done()
+        }
+    })
+    test('story id does not exist ', async (done) =>{
+        const service = {
+            content: "Hallo ik ben andres",
+            story_id: 111
+        };
+        try {
+            const response = await request.post('/newstoryblock').send(service)
+            expect(response.status).toBe(404,done())
+        } catch (error) {
+            console.log(error);
+            done()
+        }
+    })
+})
+
--- api/src/server.js
+++ api/src/server.js
@@ -1,95 +1,133 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const http = require('http');
-const Helpers = require('./utils/helpers.js')
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const Helpers = require("./utils/helpers.js");
+const { log } = require("console");
 
-const port = 3000
+const port = 3000;
 
-
-const pg = require('knex')({
-  client: 'pg',
-  version: '9.6',      
-  searchPath: ['knex', 'public'],
-  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://andres:example@localhost:5432/andres'
+const pg = require("knex")({
+  client: "pg",
+  version: "9.6",
+  searchPath: ["knex", "public"],
+  connection: process.env.PG_CONNECTION_STRING
+    ? process.env.PG_CONNECTION_STRING
+    : "postgres://andres:example@localhost:5432/andres",
 });
 
-
 const app = express();
-http.Server(app); 
-
+http.Server(app);
 
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     // to support URL-encoded bodies
-    extended: true
+    extended: true,
   })
-);  
-
-app.get('/test', (req, res) => {
-
+);
+// Show All
+app.get("/", async (req, res) => {
+  const result = await pg.select(["uuid", "title", "created_at"]).from("story");
+  res.json({
+    res: result,
+  });
+});
+app.get("/test", (req, res) => {
   res.status(200).send();
-})
+});
 
-app.get('/', async (req, res) => {
+// Show specific
+app.get("/story/:id", async (req, res) => {
   const result = await pg
-    .select(['uuid', 'title', 'created_at'])
-    .from('story')
+    .select(["uuid", "title", "created_at"])
+    .from("story")
+    .where({ uuid: req.params.uuid });
   res.json({
-      res: result
-  })
-})
-
-app.get('/story/:uuid', async (req, res) => {
+    res: result,
+  });
+});
+app.get("/storyblock/:id", async (req, res) => {
   const result = await pg
-    .select(['uuid', 'title', 'created_at'])
-    .from('story')
-    .where({uuid: req.params.uuid})
+    .select(["uuid", "content", "created_at"])
+    .from("storyblock")
+    .where({ id: req.params.id });
   res.json({
-      res: result
-  })
-})
+    res: result,
+  });
+});
 
+// Create
+app.post("/newstoryblock/", async (req, res) => {
+  const uuid = Helpers.generateUUID();
+  const getStory = await pg
+    .select(["uuid", "title", "created_at"])
+    .from("story")
+    .where({ id: req.body.story_id })
+    .then(async (data) => {
+      if (data.length >= 1) {
+        const result = await pg
+          .table("storyblock")
+          .insert({
+            uuid: uuid,
+            content: req.body.content,
+            story_id: req.body.story_id,
+          })
+          .then(async function () {
+            console.log("NEW STORYBLOCK: ", "created new storyblock");
+            res.status(200).send();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
 
+// Initialize
 async function initialiseTables() {
-  await pg.schema.hasTable('storyblock').then(async (exists) => {
+  await pg.schema.hasTable("storyblock").then(async (exists) => {
     if (!exists) {
       await pg.schema
-        .createTable('storyblock', (table) => {
+        .createTable("storyblock", (table) => {
           table.increments();
-          table.uuid('uuid');
-          table.string('content');
-          table.string('story_id');
-          table.integer('order');
+          table.uuid("uuid");
+          table.string("content");
+          table.string("story_id");
+          table.integer("order");
           table.timestamps(true, true);
         })
         .then(async () => {
-          console.log('created table storyblock');
+          console.log("created table storyblock");
         });
-
     }
   });
-  await pg.schema.hasTable('story').then(async (exists) => {
+  await pg.schema.hasTable("story").then(async (exists) => {
     if (!exists) {
       await pg.schema
-        .createTable('story', (table) => {
+        .createTable("story", (table) => {
           table.increments();
-          table.uuid('uuid');
-          table.string('title');
-          table.string('summary');
+          table.uuid("uuid");
+          table.string("title");
+          table.string("summary");
           table.timestamps(true, true);
         })
         .then(async () => {
-          console.log('created table story');
+          console.log("created table story");
           for (let i = 0; i < 10; i++) {
             const uuid = Helpers.generateUUID();
-            await pg.table('story').insert({ uuid, title: `random element number ${i}` })
+            await pg
+              .table("story")
+              .insert({ uuid, title: `random element number ${i}` });
           }
         });
-        
     }
   });
 }
-initialiseTables()
+initialiseTables();
 
 module.exports = app;
