732f1d135602f8857979ebeb363b8f173a192f0e
andreske02 <andres.vergauwen@hotmail.com>
2020-11-17 16:57:56 +0100

    fix(servers): delete backspaces

--- api/src/__tests__/integration.test.js
+++ api/src/__tests__/integration.test.js
@@ -25,8 +25,6 @@ describe('POST /test endpoint', ()=>{
                 console.log(error);
             }
             done()
-
-
         }
     })
 })
--- api/src/server.js
+++ api/src/server.js
@@ -10,7 +10,7 @@ const pg = require('knex')({
   client: 'pg',
   version: '9.6',      
   searchPath: ['knex', 'public'],
-  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
+  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://andres:example@localhost:5432/andres'
 });
 
 
